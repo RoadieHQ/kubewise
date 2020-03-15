ab002c36997cb41e1427911f182a396bf552cbfc
David Tuite <dtuite@gmail.com>
2020-03-15 22:45:03 +0000

    Remove hardcoded version number

--- bin/build_and_push.sh
+++ bin/build_and_push.sh
@@ -1,5 +1,5 @@
 set -e
 # Make sure we are authed with GCR. This step must only be run once.
 # gcloud auth configure-docker
-docker build -t us.gcr.io/larder-prod/kubewise:1.0.0 .
-docker push us.gcr.io/larder-prod/kubewise:1.0.0
+docker build -t us.gcr.io/larder-prod/kubewise:$KW_APP_VERSION .
+docker push us.gcr.io/larder-prod/kubewise:$KW_APP_VERSION
