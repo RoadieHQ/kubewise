b38e571db42b868797b27993f39fdc65a04b429f
David Tuite <dtuite@gmail.com>
2020-03-22 22:16:18 +0000

    Build and release 0.6.0

--- bin/build_and_push.sh
+++ bin/build_and_push.sh
@@ -1,5 +1,8 @@
 set -e
+
 # Make sure we are authed with GCR. This step must only be run once.
 # gcloud auth configure-docker
+
+git tag -a "v$KW_APP_VERSION" -m "AppVersion v$KW_APP_VERSION"
 docker build -t us.gcr.io/larder-prod/kubewise:$KW_APP_VERSION .
 docker push us.gcr.io/larder-prod/kubewise:$KW_APP_VERSION
