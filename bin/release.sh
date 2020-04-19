a0d4211f20bd3e68f1f90ef2815785cdaae9bab7
David Tuite <dtuite@gmail.com>
2020-04-19 22:33:47 +0100

    Add release script
    Haven't tested this yet. Might need some tweaking when it's run for
    real.

---
+++ bin/release.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# brew install yq
+# brew install goreleaser
+# brew install helm
+
+PATH_TO_HELM_REPO=~/dev/larder/helm-repo
+
+usage() {
+  echo "Usage: ./bin/release.sh {major|minor|patch}"
+  echo "Bumps the semantic version field by one for a project, tags git, releases the binaries and docker containers and builds a helm package."
+  exit 1
+}
+
+if [ -z "$1" ] ; then
+  usage
+fi
+
+BUMP_MODE="$1"
+
+./bin/bump_chart.sh $BUMP_MODE
+BUMPED_CHART_VERSION=$(yq r helm_chart/Chart.yaml version)
+BUMPED_APP_VERSION=$(yq r helm_chart/Chart.yaml appVersion)
+
+git add --all
+git commit -m "Bump: ChartVersion $BUMPED_CHART_VERSION. AppVersion $BUMPED_APP_VERSION"
+
+# The tag cannot be the app version because there will be cases 
+# where we want to make a new release even though the app has not
+# changed. Perhaps just the chart values have.
+git tag -a "$BUMPED_CHART_VERSION" -m "ChartVersion $BUMPED_CHART_VERSION"
+git push
+git push origin $BUMPED_CHART_VERSION
+
+goreleaser --rm-dist
+
+helm package ./helm_chart
+mv kubewise-*.tgz $PATH_TO_HELM_REPO
